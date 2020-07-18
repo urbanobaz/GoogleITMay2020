62c13fea93d3b25d956a34c098e674785634255b
Juan <Rodr499@gmail.com>
2020-07-18 12:52:32 -0400

    Juans commit

---
+++ juan.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import re
+
+with open('receipt.txt') as file:
+    
+    for line in file:
+        pattern = re.search(r"tel: (\(\d{3}\)\s\d{3}-\d{4})", line)
+        if pattern != None:
+            print(pattern.group(1))
+
+#print('Hello World!')
